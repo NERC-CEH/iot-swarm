ad4fc898bdb146c31e19abbbc111d627fee95d88
Lewis Chambers <lewcha@ceh.ac.uk>
2024-05-17 09:05:32 +0100

    initial commit

---
+++ src/iotdevicesimulator/__init.__.py
@@ -0,0 +1,5 @@
+import autosemver
+
+__version__ = autosemver.packaging.get_current_version(
+    project_name="iot-device-simulator"
+)
---
+++ src/tests/test_devices.py
@@ -0,0 +1,14 @@
+import unittest
+
+from iotdevicesimulator.devices import FDRIDevice
+
+
+class TestFDRISite(unittest.TestCase):
+    """Suite to test objects for simulating FDRI site objects."""
+
+    def test_instantiation(self):
+        """Test that the instance can be created and has correct attributes"""
+
+
+if __name__ == "__main__":
+    unittest.main()
