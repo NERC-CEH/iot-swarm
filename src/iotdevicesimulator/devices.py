a868938b2caf102aa3ec72cd54acbb6bc65498da
Lewis Chambers <lewcha@ceh.ac.uk>
2024-05-20 09:06:53 +0100

    Implemented basic Sensor site objects

--- src/iotdevicesimulator/devices.py
+++ src/iotdevicesimulator/devices.py
@@ -1,1 +1,49 @@
 import asyncio
+import random
+
+
+class SensorSite:
+    """Digital representation of a site used in FDRI"""
+
+    def __init__(self, site_id: str, max_cycles: int = 1) -> None:
+        self.site_id = site_id
+
+        max_cycles = int(max_cycles)
+
+        if max_cycles < 0:
+            raise ValueError(f"'max_cycles cannot be negative. Received: {max_cycles}")
+
+        self.max_cycles = max_cycles
+        self.cycle = 0
+
+    def __repr__(self):
+        return f"SensorSite({self.site_id}, {self.max_cycles})"
+
+    def __str__(self):
+        return f"Site ID: {self.site_id}, Max Cycles: {self.max_cycles}, Cycle: {self.cycle}"
+
+    async def run(self):
+
+        while True:
+            sleep = random.randrange(0, 1)
+            print(f"{str(self)} sleeping for: {sleep}")
+
+            await asyncio.sleep(sleep)
+            self.cycle += 1
+
+            if self.cycle >= self.max_cycles:
+                break
+
+
+async def main():
+
+    max_cycles = [5, 10, 15]
+
+    sites = [SensorSite(f"Site {i}", max_cycles[i]) for i in range(len(max_cycles))]
+
+    await asyncio.gather(*[x.run() for x in sites])
+
+
+if __name__ == "__main__":
+
+    asyncio.run(main())
--- src/tests/test_devices.py
+++ src/tests/test_devices.py
@@ -1,13 +1,84 @@
 import unittest
+import asyncio
+import pytest
+from iotdevicesimulator.devices import SensorSite
+from parameterized import parameterized
 
-from iotdevicesimulator.devices import FDRIDevice
 
-
-class TestFDRISite(unittest.TestCase):
+class TestSensorSiteInstantiation(unittest.TestCase):
     """Suite to test objects for simulating FDRI site objects."""
 
-    def test_instantiation(self):
+    @parameterized.expand(["ABCDE", "testsite", 12345])
+    def test_instantiation(self, site_id):
         """Test that the instance can be created and has correct attributes"""
+
+        site = SensorSite(site_id)
+
+        self.assertEqual(site.site_id, site_id)
+
+    @parameterized.expand(["ABCDE", "testsite", 12345])
+    def test__repr__(self, site_id):
+        """Tests __repr__ function returns correct string"""
+
+        site = SensorSite(site_id)
+
+        self.assertEqual(repr(site), f"SensorSite({site.site_id}, {site.max_cycles})")
+
+    @parameterized.expand(["ABCDE", "testsite", 12345])
+    def test__str__(self, site_id):
+        """Tests __str__ function returns correct string"""
+
+        site = SensorSite(site_id)
+
+        self.assertEqual(
+            str(site),
+            f"Site ID: {site.site_id}, Max Cycles: {site.max_cycles}, Cycle: {site.cycle}",
+        )
+
+    @parameterized.expand([0, 5, 10, "10"])
+    def test_max_cycle_argument(self, max_count):
+        """Tests that the max argument is set"""
+
+        site = SensorSite("SITE_ID", max_count)
+        self.assertEqual(site.max_cycles, int(max_count))
+
+    def test_max_cycle_negative_gives_error(self):
+        """Tests that negative max_count gives ValueError"""
+
+        with self.assertRaises(ValueError) as E:
+            SensorSite("SITE_ID", -10)
+
+    def test_max_cycle_non_numeric_gives_error(self):
+        """Tests that non-numeric max_cycle gives ValueError"""
+
+        with self.assertRaises(ValueError) as E:
+            SensorSite("SITE_ID", "ONE")
+
+
+class TestSensorSiteOperation(unittest.IsolatedAsyncioTestCase):
+    """Tests the active behaviour of SensorSite objects."""
+
+    @pytest.mark.asyncio
+    async def test_run_stops_after_max_cycles(self):
+        """Ensures .run() method breaks after max_cycles"""
+
+        site = SensorSite("TestSite", 5)
+        await site.run()
+
+        self.assertEqual(site.cycle, site.max_cycles)
+
+    @pytest.mark.asyncio
+    async def test_multi_instances_stop_at_max_cycles(self):
+        """Ensures .run() method breaks after max_cycles for multiple instances"""
+
+        max_cycles = [5, 6, 7]
+
+        sites = [SensorSite(f"Site {i}", max_cycles[i]) for i in range(len(max_cycles))]
+
+        await asyncio.gather(*[x.run() for x in sites])
+
+        for i, site in enumerate(sites):
+            self.assertEqual(site.cycle, max_cycles[i])
 
 
 if __name__ == "__main__":
